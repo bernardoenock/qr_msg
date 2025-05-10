4ed1486beebc36a8ed1ae92ed9e5b45623c15ed3
Bernardo Enock <91747672+bernardoenock@users.noreply.github.com>
2025-05-10 14:18:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const quotes = [
   "A maior glória em viver não está em nunca cair, mas em se levantar a cada queda. – Confúcio",
   "A jornada de mil milhas começa com um único passo. – Lao Tsé",
   "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. – Winston Churchill",
-  "Age como se o princípio da tua ação devesse tornar-se lei universal. – Immanuel Kant",
   "Não espere por uma crise para descobrir o que é importante em sua vida. – Platão",
   "Se queres ser feliz amanhã, tenta hoje mesmo. – Liang Tzu",
   "A mente é tudo. Você se torna aquilo que você pensa. – Buda",
